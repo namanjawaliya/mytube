a57fd61dc83ac0aa70146f5af5324d26bf548356
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-31 00:19:03 +0530

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,18 @@
-import { MenuIcon } from "lucide-react";
+import { FC } from "react";
+
+import { Link } from "react-router-dom";
+
 import PrimaryButton from "@/components/buttons/PrimaryButton";
+import SearchBar from "@/components/search-bar/SearchBar";
 
 import { BRAND_NAME } from "@/utils/constants";
+
 import { useAppDispatch } from "@/store/store";
 import { setSidebarState } from "@/store/reducers/globalSlice";
-import SearchBar from "../search-bar/SearchBar";
 
-const Header = () => {
+import { MenuIcon } from "lucide-react";
+
+const Header: FC = () => {
   const dispatch = useAppDispatch();
 
   const toggleSidebar = () => dispatch(setSidebarState());
@@ -20,7 +26,9 @@ const Header = () => {
         >
           <MenuIcon size={24} />
         </PrimaryButton>
-        <h1 className="font-bold uppercase text-3xl">{BRAND_NAME}</h1>
+        <h1 className="font-bold uppercase text-3xl">
+          <Link to={"/"}>{BRAND_NAME}</Link>
+        </h1>
       </div>
       <SearchBar className="hidden md:block" />
       <div>Action</div>
