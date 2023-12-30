de4295f4cc3b88ed5b5062f8769781d094e39b51
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-31 00:09:53 +0530

    added channel banner

--- src/components/channel/Channel.tsx
+++ src/components/channel/Channel.tsx
@@ -1,11 +1,40 @@
 import { FC } from "react";
 
+import { useQuery } from "react-query";
+
+import { YT_API_URI } from "@/utils/constants";
+import ChannelBanner from "./ChannelBanner";
+
 type Props = {
-  channelId: string | undefined;
+  channelId: string;
+};
+
+const fetchChannelDetails = async (channelId: string) => {
+  const endpoint = `${YT_API_URI}/channels?part=brandingSettings%2CcontentDetails%2Cid%2Clocalizations%2Csnippet%2Cstatistics%2Cstatus%2CtopicDetails&id=${channelId}&key=${
+    import.meta.env.VITE_YT_API_KEY
+  }`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data;
 };
 
 const Channel: FC<Props> = ({ channelId }) => {
-  return <div>Channel - {channelId}</div>;
+  const { data } = useQuery(["channelDetails", channelId], () =>
+    fetchChannelDetails(channelId)
+  );
+
+  const { brandingSettings } = data.items[0];
+
+  return (
+    <div className="w-full">
+      <ChannelBanner
+        src={brandingSettings?.image?.bannerExternalUrl}
+        alt={brandingSettings?.channel?.title}
+      />
+    </div>
+  );
 };
 
 export default Channel;
---
+++ src/components/channel/ChannelMetadata.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// type Props = {};
+
+const ChannelMetadata = () => {
+  return <div>ChannelMetadata</div>;
+};
+
+export default ChannelMetadata;
